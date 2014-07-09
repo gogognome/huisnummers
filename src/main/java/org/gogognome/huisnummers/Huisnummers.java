c57b9fedcdfef1d1b398a3486ee75fed4f0a796e
Sander Kooijmans <sander.kooijmans@hightechict.nl>
2014-07-09 13:12:44 +0200

    Oplossing opgeschoond en debugcode verwijderd.

--- src/main/java/org/gogognome/huisnummers/Huisnummers.java
+++ src/main/java/org/gogognome/huisnummers/Huisnummers.java
@@ -24,7 +24,6 @@ public class Huisnummers {
 
 	public static void main(String[] args) {
 		int nrThreads = Runtime.getRuntime().availableProcessors() / 2;
-		nrThreads = 1;
 		for (int i = 0; i < nrThreads; i++) {
 			new HuisnummerZoekThread((i + 1), nrThreads).start();
 		}
@@ -45,45 +44,22 @@ public class Huisnummers {
 			long m = start;
 			// Above a limit an overflow occurs in the calculations. After this limit has been reached, switch to the slower BigIntegers.
 			long limitForLongs = (long) Math.sqrt(Long.MAX_VALUE);
-			limitForLongs = 1;
 			while (m < limitForLongs) {
 				long nSquared = ((m * m) + m) / 2;
 				long n = (long) (Math.sqrt(nSquared) + 0.5);
 				if (n * n == nSquared) {
 					assertCorrect(n, m);
-					System.out.println(String.format("%d %d", n, m));
+					System.out.println(String.format("OPLOSSING GEVONDEN: %d, %d", n, m));
 				}
 				m += delta;
 			}
 
 			System.out.println("Switching to BigInteger");
 
-			// findNumbers2(BigInteger.valueOf((long) Math.sqrt(m)), BigInteger.valueOf(m), BigInteger.valueOf(delta));
-			findNumbers2(new BigInteger("7134842596"), new BigInteger("10090191164"), BigInteger.valueOf(1));
-			findNumbers(new BigInteger("7134842596"), new BigInteger("10090191164"), BigInteger.valueOf(1));
+			findNumbers(BigInteger.valueOf((long) Math.sqrt(m)), BigInteger.valueOf(m), BigInteger.valueOf(delta));
 		}
 
 		private void findNumbers(BigInteger bigN, BigInteger bigM, BigInteger bigDelta) {
-			long nextLoggingTimestamp = System.currentTimeMillis() + 60000;
-			while (true) {
-				BigInteger nSquared = ((bigM.multiply(bigM)).add(bigM)).shiftRight(1);
-				while (bigN.multiply(bigN).compareTo(nSquared) < 0) {
-					bigN = bigN.add(BigInteger.ONE);
-				}
-				if (bigN.multiply(bigN).compareTo(nSquared) == 0) {
-					System.out.println(String.format("OPLOSSING GEVONDEN: %s %s", bigN.toString(), bigM.toString()));
-				}
-				bigM = bigM.add(bigDelta);
-
-				long now = System.currentTimeMillis();
-				if (now >= nextLoggingTimestamp) {
-					System.out.println("bezig met n: " + bigN + ", m: " + bigM);
-					nextLoggingTimestamp = now + 60000;
-				}
-			}
-		}
-
-		private void findNumbers2(BigInteger bigN, BigInteger bigM, BigInteger bigDelta) {
 			long nextLoggingTimestamp = System.currentTimeMillis() + 60000;
 			while (true) {
 				BigInteger nSquaredTimesTwo = bigN.multiply(bigN).shiftLeft(1);
@@ -91,7 +67,7 @@ public class Huisnummers {
 					bigM = bigM.add(BigInteger.ONE);
 				}
 				if (bigM.multiply(bigM.add(BigInteger.ONE)).compareTo(nSquaredTimesTwo) == 0) {
-					System.out.println(String.format("OPLOSSING GEVONDEN: %s %s", bigN.toString(), bigM.toString()));
+					System.out.println(String.format("OPLOSSING GEVONDEN: %s, %s", bigN.toString(), bigM.toString()));
 				}
 				bigN = bigN.add(bigDelta);
 
@@ -103,19 +79,6 @@ public class Huisnummers {
 			}
 		}
 
-	}
-
-	private final static BigInteger sqrt(BigInteger n) {
-		BigInteger a = BigInteger.ONE;
-		BigInteger b = n.shiftRight(5).add(BigInteger.valueOf(8));
-		while (b.compareTo(a) >= 0) {
-			BigInteger mid = a.add(b).shiftRight(1);
-			if (mid.multiply(mid).compareTo(n) > 0)
-				b = mid.subtract(BigInteger.ONE);
-			else
-				a = mid.add(BigInteger.ONE);
-		}
-		return a.subtract(BigInteger.ONE);
 	}
 
 	private static void assertCorrect(long n, long m) {
